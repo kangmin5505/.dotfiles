0791c5c85f90d28b253faf05d463485972dcde4b
kangmin <kangmin5505@gmail.com>
2024-11-14 11:04:00 +0900

    update

--- bash/.config/bash/aliases.sh
+++ bash/.config/bash/aliases.sh
@@ -19,6 +19,8 @@ alias pbpaste="xsel --output --clipboard"
 alias fd="fdfind"
 alias grep="grep --color=auto"
 
-if [[ "$(uname)" == "Darwin" ]]; then
-  alias find="fd"
-fi
+# git
+GIT_LOG_FORMAT="--graph --abbrev-commit --decorate --format=format:'%C(bold blue)%h%C(reset) - %C(bold cyan)%aD%C(reset) %C(bold green)(%ar)%C(reset)%C(bold yellow)%d%C(reset)%n''          %C(white)%s%C(reset) %C(dim white)- %an%C(reset)'
+"
+alias glog="git log $GIT_LOG_FORMAT"
+alias gloga="git log --all $GIT_LOG_FORMAT"
